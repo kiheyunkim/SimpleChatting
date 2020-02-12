fe5ee96064793efe7b02dd882433dc177581aa6b
kiheyunkim <kiheyunkim@gmail.com>
2020-02-12 15:10:50 +0900

    first commit

---
+++ Server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const http = require('http').createServer(app);
+let io = require('socket.io')(http);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/',(request,response)=>{
+    response.redirect('/index.html');
+});
+
+http.listen(3001,()=>{
+    console.log('Chatting Server Open');
+})
+
+io.on('connection',(socket)=>{
+    console.log(socket.id + ': random user connected');
+    socket.on('message',(packet)=>{
+        console.log(packet);
+        io.emit('message',packet)
+    });
+
+    socket.on('disconnect',(reason)=>{
+        console.log('reason: '+ reason + " disconnected " + socket.id);
+    });
+});
+
+
